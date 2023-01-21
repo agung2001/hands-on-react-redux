9441ee235d6a41e6508cf08f6e186de54c924e9d
Agung Sundoro <muhagung2001@gmail.com>
2023-01-21 11:17:32 +0700

    feat: finish project redux-saga-with-redux-toolkit

--- projects/redux-saga-with-redux-toolkit/src/main.jsx
+++ projects/redux-saga-with-redux-toolkit/src/main.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+/** Setup Redux */
+import {Provider} from 'react-redux'
+import createSagaMiddleware from 'redux-saga';
+import {configureStore} from '@reduxjs/toolkit'
+import catsReducer from './catSlice.js'
+import catSaga from './catSaga.js'
+
+const sagaMiddleware = createSagaMiddleware();
+const store = configureStore({
+    reducer: {
+        cats: catsReducer
+    },
+    middleware: [sagaMiddleware]
+})
+sagaMiddleware.run(catSaga)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
 )
